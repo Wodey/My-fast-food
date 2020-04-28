07d23f712558e765de80ba59f4f85a1c2c002b44
Ivan Newwest <ivannewest@gmail.com>
2020-04-28 15:32:26 +0300

    i started project + Navigation component

---
+++ src/components/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default () => (
+  <div className="navigation">
+    <a className="navigation-child">Bienvenidos</a>
+    <a className="navigation-child">Menu</a>
+    <a className="navigation-child"><img src="/logo.svg" className="navigation-logo"/></a>
+    <a className="navigation-child">Contact</a>
+    <span className="navigation-child">We're open now</span>
+  </div>
+);
