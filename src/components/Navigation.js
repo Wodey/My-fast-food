10f42ab23c96133776fa35bd397a3a64f9d096a7
Ivan Newwest <ivannewest@gmail.com>
2020-05-02 21:20:04 +0300

    + smooth scroll

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -19,10 +19,31 @@ export default () => {
   })
   return (
     <div className={`navigation ${transformNav ? "navigation-transform" : ""}`}>
-      <a className="navigation-child" href="#bienvenidos">Bienvenidos</a>
-      <a className="navigation-child" href="#menu">Menu</a>
+      <a className="navigation-child" href="#bienvenidos" onClick={(e) => {
+        e.preventDefault();
+        window.scroll({
+          top: 0,
+          left: 0,
+          behavior: 'smooth'
+        })
+      }}>Bienvenidos</a>
+      <a className="navigation-child" href="#menu"  onClick={(e) => {
+        e.preventDefault();
+        window.scroll({
+          top: 1700,
+          left: 0,
+          behavior: 'smooth'
+        })
+      }}>Menu</a>
       <a className="navigation-child"><img src="/logo.svg" className="navigation-logo"/></a>
-      <a className="navigation-child" href="#contact">Contact</a>
+      <a className="navigation-child" href="#contact"  onClick={(e) => {
+        e.preventDefault();
+        window.scroll({
+          top: 4000,
+          left: 0,
+          behavior: 'smooth'
+        })
+      }}>Contact</a>
       <span className="navigation-child">We're open now</span>
     </div>
   );
