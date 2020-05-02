c1628c649b387d74b8954670a407b57f71a24213
Ivan Newwest <ivannewest@gmail.com>
2020-05-02 14:11:30 +0300

    + i have rewrited Gallery component , now it work how i wanted it

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,41 +1,40 @@
-import React,{useState} from 'react';
+import React,{useState,useRef} from 'react';
 
 export default () => {
-    const [currentImage, setCurrentImage] = useState(3);
-    const [change, setChange] = useState(false);
-    const swipeRight = () => {
-      setChange("right");
-      if(currentImage < 8) {
-        setCurrentImage(currentImage + 1);
-      } else {
-        setCurrentImage(3);
+    const [counter, setCounter] = useState(0);
+    const [transition, setTransition] = useState(.6);
+    const handleLeft = () => {
+      if(counter <= 0) {
+        setTransition(1.2);
+        setCounter(5);
+        return;
       }
-      setTimeout(() => {
-        setChange(false);
-      },1000)
+      setTransition(.6);
+      setCounter(counter - 1);
     };
 
-    const swipeLeft = () => {
-        setChange("left");
-        if(currentImage > 3) {
-          setCurrentImage(currentImage - 1);
-        } else {
-          setCurrentImage(8);
-        }
-        setTimeout(() => {
-          setChange(false);
-        },1000)
+    const handleRight = () => {
+      if(counter >= 5) {
+        setTransition(1.2);
+        setCounter(0);
+        return;
+      }
+      setTransition(.6);
+      setCounter(counter + 1);
     };
-
-
     return (
-    <div className="gallery">
-      <span>Gallery</span>
-      <div className="gallery-wrapper">
-        <img className="gallery-wrapper-click" src="/left.svg" alt="left" onClick={swipeLeft} />
-        <img className={`gallery-wrapper-image ${change ? change : ""}`} src={`/${currentImage}.jpg`}  alt="gallery"/>
-        <img className="gallery-wrapper-click" src="/right.svg" alt="right" onClick={swipeRight} />
+      <div className="gallery">
+        <img className="gallery-btn-left gallery-btn" src="/left.svg" onClick={handleLeft} />
+        <div className="gallery-carousel" style={{"transform": `translateX(${-counter * 100}%)`,
+                                                  "transition": `${transition}s`}} >
+          <img src="/3.jpg"/>
+          <img src="/4.jpg"/>
+          <img src="/5.jpg"/>
+          <img src="/6.jpg"/>
+          <img src="/7.jpg"/>
+          <img src="/8.jpg"/>
+        </div>
+        <img className="gallery-btn-right gallery-btn" src="/right.svg" onClick={handleRight} />
       </div>
-    </div>
   );
 };
