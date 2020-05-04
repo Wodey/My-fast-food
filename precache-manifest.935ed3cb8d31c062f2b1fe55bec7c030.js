8f50f271ae4b741921333d0794e8e2643bc8a39b
Ivan Newwest <ivannewest@gmail.com>
2020-05-04 10:09:13 +0300

    Updates

---
+++ precache-manifest.935ed3cb8d31c062f2b1fe55bec7c030.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddfcd990adee81feaf649d8c9d94d8c9",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "43972a0c5c4faa347298",
+    "url": "/my-app/static/css/main.ad4a4df7.chunk.css"
+  },
+  {
+    "revision": "10ff31813c282487f261",
+    "url": "/my-app/static/js/2.fc8d8766.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.fc8d8766.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43972a0c5c4faa347298",
+    "url": "/my-app/static/js/main.1bd06d87.chunk.js"
+  },
+  {
+    "revision": "cae875712470577a195e",
+    "url": "/my-app/static/js/runtime-main.22966c9c.js"
+  }
+]);
